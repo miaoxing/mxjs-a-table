afb6683d223dc0c65cbb7f67408d7120e525115e
twinh <twinhuang@qq.com>
2023-03-17 09:18:04 +0800

    feat: 带参数的链接生成 `permission` 简化权限判断

--- CTableDeleteLink.js
+++ CTableDeleteLink.js
@@ -2,7 +2,11 @@ import PropTypes from 'prop-types';
 import TableDeleteLink from './TableDeleteLink';
 import curUrl from '@mxjs/cur-url';
 
-const CTableDeleteLink = ({id, ...props}) => <TableDeleteLink href={curUrl.apiShow(id)} {...props} />;
+const CTableDeleteLink = ({id, ...props}) => {
+  return (
+    <TableDeleteLink permission={curUrl.to('[id]/delete')} href={curUrl.apiShow(id)} {...props} />
+  );
+};
 
 CTableDeleteLink.propTypes = {
   id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
