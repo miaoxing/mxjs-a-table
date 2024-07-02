05c5944c6d53a60136a02806bc30755980aa3f0e
twinh <twinhuang@qq.com>
2024-07-02 20:28:33 +0800

    fix: `export default from` 改为标准语法

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-export Table from './Table';
-export TableProvider, {withTable, TableContext} from './TableProvider';
-export TableStatusCheckbox from './TableStatusCheckbox';
-export TableDeleteLink from './TableDeleteLink';
-export CTableDeleteLink from './CTableDeleteLink';
-export TableActions from './TableActions';
-export TableTabs from './TableTabs';
-export {useTable} from './util';
+import Table from './Table';
+import TableProvider, { withTable, TableContext } from './TableProvider';
+import TableStatusCheckbox from './TableStatusCheckbox';
+import TableDeleteLink from './TableDeleteLink';
+import CTableDeleteLink from './CTableDeleteLink';
+import TableActions from './TableActions';
+import TableTabs from './TableTabs';
+import { useTable } from './util';
+
+export {
+  Table,
+  TableProvider,
+  TableContext,
+  TableStatusCheckbox,
+  TableDeleteLink,
+  CTableDeleteLink,
+  TableActions,
+  TableTabs,
+  useTable,
+  withTable,
+};
