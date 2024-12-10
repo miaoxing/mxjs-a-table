c2aba55c96d33f1df24e01148c767dc4a4ffe925
twinh <twinhuang@qq.com>
2024-12-10 21:07:13 +0800

    fix: 链接按钮移除不再使用的 `getPermissions` 方法

--- CTableDeleteLink.jsx
+++ CTableDeleteLink.jsx
@@ -2,18 +2,14 @@ import PropTypes from 'prop-types';
 import TableDeleteLink from './TableDeleteLink';
 import curUrl from '@mxjs/cur-url';
 
-const getPermission = () => curUrl.to('[id]/delete');
-
 const CTableDeleteLink = ({id, ...props}) => {
   return (
-    <TableDeleteLink permission={getPermission()} href={curUrl.apiShow(id)} {...props} />
+    <TableDeleteLink permission={curUrl.to('[id]/delete')} href={curUrl.apiShow(id)} {...props} />
   );
 };
 
 CTableDeleteLink.propTypes = {
   id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
 };
-
-CTableDeleteLink.getPermission = getPermission;
 
 export default CTableDeleteLink;
