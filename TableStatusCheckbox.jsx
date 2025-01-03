b6cd4dbb6ec0fe6eedf13b987cabbb7dd0f610ac
twinh <twinhuang@qq.com>
2025-01-03 22:10:12 +0800

    refactor(table): 将 TableStatusCheckbox 改为函数式组件

--- TableStatusCheckbox.jsx
+++ TableStatusCheckbox.jsx
@@ -1,103 +1,90 @@
-import React from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
-import {withTable} from './TableProvider';
+import { withTable } from './TableProvider';
 import $ from 'miaoxing';
 import curUrl from '@mxjs/cur-url';
-import {Checkbox} from 'antd';
-import {setValue, getValue} from 'rc-field-form/lib/utils/valueUtil';
+import { Checkbox } from 'antd';
+import { setValue, getValue } from 'rc-field-form/lib/utils/valueUtil';
 
-// 记录checkbox状态，以免被外部重置
+// 记录 checkbox 状态，以免被外部重置
 const store = {};
 
-class StatusCheckbox extends React.Component {
-  static propTypes = {
-    mode: PropTypes.oneOf(['patch', 'toggle']),
-    url: PropTypes.string,
-    name: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.array,
-    ]).isRequired,
-    row: PropTypes.shape({
-      id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
-    }),
-    table: PropTypes.shape({
-      reload: PropTypes.func.isRequired,
-    }).isRequired,
+const getName = (name) => {
+  return Array.isArray(name) ? name : [name];
+};
+const getInitialValue = (row, name) => {
+  if (typeof store[row.id] !== 'undefined') {
+    return store[row.id];
   }
 
-  static defaultProps = {
-    mode: 'patch',
+  let value = getValue(row, getName(name));
+  return value === '0' ? false : !!value;
+};
+
+const getUrl = (url, rowId) => {
+  return url || curUrl.apiItem(rowId);
+};
+
+const getMethod = (mode, checked) => {
+  if (mode === 'patch') {
+    return 'PATCH';
   }
+  return checked ? 'DELETE' : 'PUT';
+};
 
-  state = {
-    checked: this.getValue(),
-  };
+const getData = (mode, rowId, name, checked) => {
+  if (mode === 'patch') {
+    return Object.assign(
+      { id: rowId },
+      setValue({}, getName(name), checked)
+    );
+  }
+  return {};
+};
 
-  handleChange = () => {
-    const checked = !this.state.checked;
+const StatusCheckbox = ({ mode = 'patch', url, name, row, table }) => {
+  const [checked, setChecked] = useState(getInitialValue(row, name));
+
+  const handleChange = (e) => {
+
+    const newChecked = e.target.checked;
 
     // 更改时，将状态记录起来
-    store[this.props.row.id] = checked;
+    store[row.id] = newChecked;
 
-    this.setState({checked: checked});
+    setChecked(newChecked);
     $.http({
-      url: this.getUrl(),
-      method: this.getMethod(),
-      data: this.getData(),
-    }).then(({ret}) => {
+      url: getUrl(url, row.id),
+      method: getMethod(mode, newChecked),
+      data: getData(mode, row.id, name, newChecked),
+    }).then(({ ret }) => {
       $.ret(ret);
 
       // 重载数据，加载新的状态
-      this.props.table.reload();
+      table.reload();
 
       // 更改后,删除状态
-      delete store[this.props.row.id];
+      delete store[row.id];
     });
   };
 
-  /**
-   * @todo 支持自动生成 delete 地址
-   */
-  getUrl() {
-    return this.props.url || curUrl.apiItem(this.props.row.id);
-  }
+  return <Checkbox checked={checked} onChange={handleChange} />;
+};
 
-  getMethod() {
-    if (this.props.mode === 'patch') {
-      return 'PATCH';
-    }
-    return this.state.checked ? 'DELETE' : 'PUT';
-  }
-
-  getData() {
-    if (this.props.mode === 'patch') {
-      return Object.assign(
-        {id: this.props.row.id},
-        setValue({}, this.getName(), !this.state.checked)
-      );
-    }
-    return {};
-  }
-
-  getValue() {
-    // 如果有更改过，显示更改过的状态
-    if (typeof store[this.props.row.id] !== 'undefined') {
-      return store[this.props.row.id];
-    }
-
-    let value = getValue(this.props.row, this.getName());
-    // 兼容数据库返回
-    return value === '0' ? false : !!value;
-  }
-
-  getName() {
-    return Array.isArray(this.props.name) ? this.props.name : [this.props.name];
-  }
-
-  render() {
-    return <Checkbox checked={this.state.checked} onChange={this.handleChange}/>;
-  }
-}
+StatusCheckbox.propTypes = {
+  mode: PropTypes.oneOf(['patch', 'toggle']),
+  url: PropTypes.string,
+  name: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.array,
+  ]).isRequired,
+  row: PropTypes.shape({
+    id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+  }),
+  table: PropTypes.shape({
+    reload: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 const TableStatusCheckbox = withTable(StatusCheckbox);
 
